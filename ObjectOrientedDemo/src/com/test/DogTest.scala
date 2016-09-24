29e00c30184d965e71a3b404065b1824a6b72572
Dax1n <568275045@qq.com>
2016-09-24 14:19:26 +0800

    add DogTest

---
+++ ObjectOrientedDemo/src/com/test/DogTest.scala
@@ -0,0 +1,14 @@
+package com.test
+
+import com.daxin.Person
+//import com.daxin.Dog
+
+object PersonTest {
+  def main(args: Array[String]): Unit = {
+    val p=new Person
+    
+    //val dog=new Dog //报错：not found: type Dog  ，就算是导包也不可以。
+    
+    
+  }
+}
